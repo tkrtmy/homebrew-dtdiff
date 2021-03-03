bf8e6839cd1e3cda97b7f041979633ea1d63aff4
tkrtmy <tkrtmy1333@gmail.com>
2021-03-03 23:44:43 +0900

    v0.4.0

--- dtdiff.rb
+++ dtdiff.rb
@@ -1,25 +1,17 @@
-VERSION = 'v0.3.0'
+VERSION = 'v0.4.0'
 
 class Dtdiff < Formula
   desc "calculate date time diff"
   version "#{VERSION}"
   homepage "https://github.com/tkrtmy/dtdiff"
   head "https://github.com/tkrtmy/dtdiff.git"
+  license "MIT"
 
-  if Hardware::CPU.is_64_bit?
-    url "https://github.com/tkrtmy/dtdiff/releases/download/#{VERSION}/dtdiff_darwin_amd64"
-    sha256 "0a8f9dd4bba52ddf1643bb33e4ca29a6806528fc7bfc97e720d7d1d89b78162c"
-  else
-    url "https://github.com/tkrtmy/dtdiff/releases/download/#{VERSION}/dtdiff_darwin_386"
-    sha256 "e83c61317dfca92d6f7bf962940592ccba7f6ba60083dbfa77da0601982f9293"
-  end
+  url "https://github.com/tkrtmy/dtdiff/releases/download/v0.4.0/dtdiff_darwin_amd64.tar.gz"
+  sha256 "5e8ecd24de576faf15065b32a14d58b6e1572e2195601713f4e39beaeef2530a"
 
   def install
-    if Hardware::CPU.is_64_bit?
-      bin.install "dtdiff_darwin_amd64" => "dtdiff"
-    else
-      bin.install "dtdiff_darwin_386" => "dtdiff"
-    end
+    bin.install "dtdiff"
   end
 
   def caveats
